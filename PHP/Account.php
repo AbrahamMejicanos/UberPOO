6e4a09b23f8835b967b29c8732e9d945d9592f18
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-18 15:34:12 -0600

    Creacion de funciones y constructores para Account y Car

--- PHP/Account.php
+++ PHP/Account.php
@@ -1,5 +1,11 @@
 <?php
     class Account{
         public $id, $document, $email, $password;
+
+        public function __construct($name, $document)
+        {
+            $this->name = $name;
+            $this->document = $document;
+        }
     }
 ?>
--- PHP/Car.php
+++ PHP/Car.php
@@ -1,5 +1,17 @@
 <?php
     class Car{
         public $id, $driver, $passenger;
+
+        public function __construct($licence, $driver)
+        {
+            $this->license = $licence;
+            $this->driver = $driver;
+        }
+
+        public function PrintDataCar(){
+            echo "License: {$this->license},
+            Driver Name: {$this->driver->name},
+            Driver Document: {$this->driver->document}";
+        }
     }
 ?>

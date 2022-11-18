6d48a642b58be7eacc132f38e779bb36b6dd82b4
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-18 13:47:44 -0600

    Creacion de Constructor Car y funcion imprimirDatos

--- Java/Car.java
+++ Java/Car.java
@@ -1,4 +1,16 @@
 class Car {
-    Integer id, passenger;
-    String license, driver;
+    private Integer id, passenger;
+    private String license, driver;
+
+    public Car(Integer passenger, String license, String driver){
+        this.passenger = passenger;
+        this.license = license;
+        this.driver = driver;
+    }
+
+    public void mostrarDatos(){
+        System.out.println("Licencia: " + license);
+        System.out.println("Conductor: " + driver);
+        System.out.println("Para: " + passenger + " pasajeros");
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Car car = new Car(4, "AMQ123", "Andres Herrera");
+        car.mostrarDatos();
     }
 }

74c7bb476ebdd112b0e71b093df24cfe0c2461ca
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-21 18:25:47 -0600

    Creacion de Driver.java

---
+++ Java/Driver.java
@@ -0,0 +1,7 @@
+import javax.swing.text.Document;
+
+class Driver extends Account{
+    public Driver(String name, String document, String email, String password){
+        super(name, document, email, password);
+    }
+}

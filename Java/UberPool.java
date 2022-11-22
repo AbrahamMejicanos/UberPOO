38497152855b5573337d00d63e7e9b042481466d
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-21 23:18:18 -0600

    Se hizo polimorfismo en UberX y cambio en parametros de UberPool

--- Java/UberPool.java
+++ Java/UberPool.java
@@ -2,8 +2,8 @@ class UberPool extends Car{
     String brand;
     String model;
 
-    public UberPool(String license, Account driver, Integer passenger, String brand, String model){
-        super(license, driver, passenger);
+    public UberPool(String license, Account driver, String brand, String model){
+        super(license, driver);
         this.brand = brand;
         this.model = model;
     }
--- Java/UberX.java
+++ Java/UberX.java
@@ -2,9 +2,16 @@ class UberX extends Car{
     String brand;
     String model;
 
-    public UberX(String license, Account driver, Integer passenger, String brand, String model){
-        super(license, driver, passenger);
+    public UberX(String license, Account driver, String brand, String model){
+        super(license, driver);
         this.brand = brand;
         this.model = model;
+    }
+
+    
+    @Override
+    public void mostrarDatos() {
+        super.mostrarDatos();
+        System.out.println("Marca: " + brand + " | Modelo: " + model);
     }
 }

3e52654af53d3086eb306c47dde42cce2e1054d7
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-21 23:15:19 -0600

    Modificacion en modificador de acceso, cambio a protected para uso de polimorfismo

--- Java/Car.java
+++ Java/Car.java
@@ -1,29 +1,32 @@
 class Car {
-    private Integer id, passenger;
+    private Integer id;
     private String license;
     private Account driver;
+    protected Integer passenger;
+    protected String mensajeError;
 
-    public Car(String license, Account driver, Integer passenger){
+    public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
-        this.passenger = passenger;
-    }
-
-    public Integer getPassenger(){
-        return passenger;
     }
 
     public void setPassenger(Integer x){
-        if(x == 4){
+        if(x >= 1 && x <= 4){
             this.passenger = x; 
-        }else{
-            System.out.println("Necesitas almenos 4 pasajeros");
+        }else if(x > 4){
+            mensajeError = "No puedes subir mas de 4 pasajeros";
+        }else if(x < 1){
+            mensajeError = "El minimo de pasajeros es 1";
         }
     }
 
     public void mostrarDatos(){
-        System.out.println("Licencia: " + license);
-        System.out.println("Conductor: " + driver.stName());
-        System.out.println("Pasajeros: " + passenger);
+        if(passenger != null){
+            System.out.println("Licencia: " + license);
+            System.out.println("Conductor: " + driver.gtName());
+            System.out.println("Pasajeros: " + passenger);
+        }else{
+            System.out.println("Error: " + mensajeError);
+        }
     }
 }
